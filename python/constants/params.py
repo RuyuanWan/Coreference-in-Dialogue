992b9215fa860792f95820436d064ef8e05f4219
RuyuanWan <30328069+RuyuanWan@users.noreply.github.com>
2021-11-17 15:12:03 -0600

    Add files via upload

---
+++ python/constants/params.py
@@ -0,0 +1,16 @@
+class CorefParams:
+    nb_fltrs = 280
+    nb_epoch = 5
+    #batch_size = 128
+    batch_size = 10
+    gpu = []
+    eval_only = False
+
+
+class LinkingParams:
+    nb_fltrs = 280
+    nb_epoch = 10
+    #batch_size = 128
+    batch_size = 10
+    gpu = []
+    eval_only = False
