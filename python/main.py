1fb9163999427447df230587d794a3b455a6c6c4
RuyuanWan <30328069+RuyuanWan@users.noreply.github.com>
2021-11-17 15:16:51 -0600

    Add files via upload

---
+++ python/main.py
@@ -0,0 +1,12 @@
+from plural_resolution import PluralResolutionDemo
+import os
+
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+def main():
+    demo = PluralResolutionDemo()
+    demo.exe()
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/util/loadutils.py
@@ -0,0 +1,32 @@
+import fasttext
+from gensim import models
+import spacy
+import json
+
+from constants.paths import Paths
+from util.readers import GenderDataReader, DictionaryReader
+
+
+def load_json_from_path(path):
+    with open(path, "r") as fin:
+        return json.load(fin)
+
+
+def load_word_vecs():
+    return fasttext.load_model(Paths.Resources.Fasttext300d)
+#    return models.KeyedVectors.load_word2vec_format()
+#    return spacy.load('en_core_web_md')
+
+def load_gender_data():
+    w2g_in = open(Paths.Resources.GenderData, 'rb')
+    return GenderDataReader.load(w2g_in, True, True)
+
+
+def load_animate_data():
+    ani_in = open(Paths.Resources.AnimateUnigram, 'rb')
+    return DictionaryReader.load_string_set(ani_in)
+
+
+def load_inanimate_data():
+    ina_in = open(Paths.Resources.InanimateUnigram, 'rb')
+    return DictionaryReader.load_string_set(ina_in)
